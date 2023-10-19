0169f3b17065cb7f503b08d89751763f989b89c8
hL <1263164021@qq.com>
2023-10-19 20:52:11 +0800

    改为智能指针

--- src/mytree.cpp
+++ src/mytree.cpp
@@ -3,92 +3,103 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
-class FileNode{
+#include<memory>
+// 文件节点基类
+class FileNode {
 protected:
     std::string fileName;
+
 public:
-    FileNode(const std::string&name):fileName(name){}
-    virtual void add(FileNode* fileNode)=0;
-    virtual void display(int depth)=0;
+    FileNode(const std::string& name) : fileName(name) {}
+
+    // 纯虚函数，派生类必须实现
+    virtual void add(const std::shared_ptr<FileNode>& fileNode) = 0;
+    virtual void add(std::shared_ptr<FileNode>&& fileNode) = 0;
+    virtual void display(int depth) = 0;
 };
 
-class File:public FileNode{
+// 文件类，继承自文件节点基类
+class File : public FileNode {
 public:
-    File(const std::string& name):FileNode(name){}
-    void add(FileNode* fileNode) override{
-         printf("Cannot add to a File");
+    File(const std::string& name) : FileNode(name) {}
+
+    // 不允许在文件中添加文件或文件夹
+    void add(const std::shared_ptr<FileNode>& fileNode) override {
+        printf("无法添加到文件中");
     }
-    void display(int depth){
-        // std::string out=std::string(depth,'-');
-        // std::cout<<out<<fileName<<std::endl;
-        // std::string out=std::string(depth/2,' ');
-        // out+='|';
-        // out+=std::string(2,'-');
-        // std::cout<<out<<fileName<<std::endl;
+
+    void add(std::shared_ptr<FileNode>&& fileNode) override {
+        printf("无法添加到文件中");
+    }
+
+    // 显示文件信息
+    void display(int depth) override {
         std::string indentation(depth, ' ');
         std::cout << indentation << "|-- " << fileName << std::endl;
     }
 };
 
-class Folder: public FileNode{
+// 文件夹类，继承自文件节点基类
+class Folder : public FileNode {
 private:
-    std::vector<FileNode*>children;
+    std::vector<std::shared_ptr<FileNode>> children;
+
 public:
-    Folder(const std::string& name):FileNode(name){}
-    void add(FileNode* fileNode) override{
-         children.emplace_back(fileNode);
+    // 构造函数
+    Folder(const std::string& name) : FileNode(name) {}
+
+    Folder(std::string&& name) : FileNode(std::move(name)) {}
+
+    // 允许在文件夹中添加文件或文件夹
+    void add(const std::shared_ptr<FileNode>& fileNode) override {
+        children.emplace_back(fileNode);
     }
-    void display(int depth){
-        // std::string out=std::string(depth/2,' ');
-        // out+='|';
-        // out+=std::string(2,'-');
-        // std::cout<<out<<fileName<<std::endl;
-        // for(auto it:children){
-        //     it->display(depth+2);
-        // }
 
+    void add(std::shared_ptr<FileNode>&& fileNode) override {
+        children.emplace_back(std::move(fileNode));
+    }
 
+    // 显示文件夹及其子项信息
+    void display(int depth) override {
         std::string indentation(depth, ' ');
         std::cout << indentation << "|-- " << fileName << "/" << std::endl;
         for (auto it : children) {
-            it->display(depth + 3); // Adjust the depth for sub-items
+            it->display(depth + 3);
         }
     }
 };
 
+// 文件树类
 class FileTree{
 private:
-
-    void getDirList(const std::string& directoryPath,FileNode*& curFolder);
+    // 递归获取目录列表
+    void getDirList(const std::string& directoryPath,std::shared_ptr<FileNode>&curFolder);
 public:
+    // 构建并显示文件树
     void tree(std::string folderPath="."){
-        FileNode* root=new Folder(folderPath);
-    
+        std::shared_ptr<FileNode>root(new Folder(folderPath));
         getDirList(folderPath,root);
         root->display(0);
     }
 };
 
-
-
-void FileTree::getDirList(const std::string& directoryPath,FileNode*& curFolder){
+//递归得到文件列表
+void FileTree::getDirList(const std::string& directoryPath,std::shared_ptr<FileNode>&curFolder){
     DIR* dir = opendir(directoryPath.c_str());
     if (dir) {
         struct dirent* entry;
         while ((entry = readdir(dir)) != nullptr) {
             if (entry->d_type == DT_REG) {
                 //文件
-                // std::cout << "文件名: " << entry->d_name << std::endl;
-                curFolder->add(new File(std::string(entry->d_name)));
+                curFolder->add(std::make_shared<File>(std::string(entry->d_name)));
             }else{
                 //文件夹
                 auto folder_name=std::string(entry->d_name);
                 if(folder_name==".."||folder_name=="."){
                     continue;
                 }
-                // std::cout << "文件夹名: " << folder_name<< std::endl;
-                FileNode* thisFolder=new struct Folder(folder_name);
+
+                std::shared_ptr<FileNode>thisFolder(new Folder(folder_name));
                 curFolder->add(thisFolder);
                 getDirList(directoryPath+'\\'+folder_name,thisFolder);
             }
@@ -100,12 +111,10 @@ void FileTree::getDirList(const std::string& directoryPath,FileNode*& curFolder)
 }
 
 int main(int argc,char**argv){
-    FileTree* fileTree=new FileTree();
+    std::shared_ptr<FileTree>fileTree;
     if(argc<2){
-        fileTree->tree("..");
+        fileTree->tree(".");
     }else{
         fileTree->tree(std::string(argv[1]));
     }
-    
-    
 }
